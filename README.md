2d8a102dd7399a243f11b13314c299e509cc4ef1
Alireza <alirezatm@ymail.com>
2023-03-13 23:19:45 +0330

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+RTL8821CS driver for linux
+Successfully tested on V3S + Fnlink 6621 module
+
+stolen off [sophpi huashan project](https://github.com/sophgo/sophpi-huashan)
+
+cross compilation example:
+make ARCH=arm CROSS_COMPILE=arm-linux-gnueabihf- KERNEL_DIR=../../buildroot-2022.08.1/output/build/linux-5.4.217/
---
+++ rtl8821cs/core/monitor/.rtw_radiotap.o.cmd
@@ -0,0 +1,24 @@
+cmd_/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o := arm-linux-gnueabihf-gcc -Wp,-MD,/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/.rtw_radiotap.o.d  -nostdinc -isystem /usr/lib/gcc-cross/arm-linux-gnueabihf/9/include -I./arch/arm/include -I./arch/arm/include/generated  -I./include -I./arch/arm/include/uapi -I./arch/arm/include/generated/uapi -I./include/uapi -I./include/generated/uapi -include ./include/linux/kconfig.h -include ./include/linux/compiler_types.h -D__KERNEL__ -mlittle-endian -Wall -Wundef -Werror=strict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -fshort-wchar -fno-PIE -Werror=implicit-function-declaration -Werror=implicit-int -Werror=return-type -Wno-format-security -std=gnu89 -fno-dwarf2-cfi-asm -fno-ipa-sra -mabi=aapcs-linux -mfpu=vfp -funwind-tables -marm -Wa,-mno-warn-deprecated -D__LINUX_ARM_ARCH__=7 -march=armv7-a -msoft-float -Uarm -fno-delete-null-pointer-checks -Wno-frame-address -Wno-format-truncation -Wno-format-overflow -Wno-address-of-packed-member -O2 --param=allow-store-data-races=0 -Wframe-larger-than=1024 -fstack-protector-strong -Wimplicit-fallthrough -Wno-unused-but-set-variable -Wno-unused-const-variable -fomit-frame-pointer -fno-var-tracking-assignments -Wdeclaration-after-statement -Wvla -Wno-pointer-sign -Wno-stringop-truncation -Wno-array-bounds -Wno-stringop-overflow -Wno-restrict -Wno-maybe-uninitialized -fno-strict-overflow -fno-merge-all-constants -fmerge-constants -fno-stack-check -fconserve-stack -Werror=date-time -Werror=incompatible-pointer-types -Werror=designated-init -fmacro-prefix-map=./= -Wno-packed-not-aligned -O1 -Wno-unused-variable -Wno-date-time -I/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/include -I/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/platform -I/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/hal/btc -DCONFIG_AP_MODE -DCONFIG_P2P -DCONFIG_MP_INCLUDED -DCONFIG_POWER_SAVING -DCONFIG_BT_COEXIST -DCONFIG_EFUSE_CONFIG_FILE -DEFUSE_MAP_PATH=\"/system/etc/wifi/wifi_efuse_8821cs.map\" -DWIFIMAC_PATH=\"/data/wifimac.txt\" -DCONFIG_LOAD_PHY_PARA_FROM_FILE -DREALTEK_CONFIG_PATH=\"/lib/firmware/\" -DCONFIG_TXPWR_BY_RATE=1 -DCONFIG_TXPWR_BY_RATE_EN=1 -DCONFIG_TXPWR_LIMIT=1 -DCONFIG_TXPWR_LIMIT_EN=0 -DCONFIG_RTW_ADAPTIVITY_EN=0 -DCONFIG_RTW_ADAPTIVITY_MODE=0 -DCONFIG_IEEE80211W -DCONFIG_LAYER2_ROAMING -DCONFIG_ROAMING_FLAG=0x3 -DHIGH_ACTIVE_HST2DEV=0 -DCONFIG_RTW_SDIO_PM_KEEP_POWER -DCONFIG_BR_EXT '-DCONFIG_BR_EXT_BRNAME="'br0'"' -DCONFIG_RTW_NAPI -DCONFIG_RTW_GRO -DCONFIG_RTW_NETIF_SG -DCONFIG_RTW_DEBUG -DRTW_LOG_LEVEL=4 -DCONFIG_PROC_DEBUG -DCONFIG_RTW_UP_MAPPING_RULE=0 -DDM_ODM_SUPPORT_TYPE=0x04 -DCONFIG_LITTLE_ENDIAN -DCONFIG_IOCTL_CFG80211 -DRTW_USE_CFG80211_STA_EVENT -I/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/hal/phydm -DCONFIG_RTL8821C -I/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/crypto  -DMODULE  -DKBUILD_BASENAME='"rtw_radiotap"' -DKBUILD_MODNAME='"8821cs"' -c -o /home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o /home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.c
+
+source_/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o := /home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.c
+
+deps_/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o := \
+    $(wildcard include/config/wifi/monitor.h) \
+  include/linux/kconfig.h \
+    $(wildcard include/config/cpu/big/endian.h) \
+    $(wildcard include/config/booger.h) \
+    $(wildcard include/config/foo.h) \
+  include/linux/compiler_types.h \
+    $(wildcard include/config/have/arch/compiler/h.h) \
+    $(wildcard include/config/enable/must/check.h) \
+    $(wildcard include/config/optimize/inlining.h) \
+    $(wildcard include/config/cc/has/asm/inline.h) \
+  include/linux/compiler_attributes.h \
+  include/linux/compiler-gcc.h \
+    $(wildcard include/config/arm64.h) \
+    $(wildcard include/config/retpoline.h) \
+    $(wildcard include/config/arch/use/builtin/bswap.h) \
+
+/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o: $(deps_/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o)
+
+$(deps_/home/alireza/Downloads/UntitledFolder/sdio_rtl8821cs/rtl8821cs/core/monitor/rtw_radiotap.o):
